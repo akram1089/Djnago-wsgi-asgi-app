f683092d57c155fc3fa2b84266b74791d503c52f
Tufail Akram <72789060+akram1089@users.noreply.github.com>
2024-02-06 11:37:01 +0530

    updated the admins.py

--- home/admin.py
+++ home/admin.py
@@ -8,8 +8,10 @@ class ExchangeDataAdmin(admin.ModelAdmin):
     list_display = ('exchange', 'symbol',)
     search_fields = ('exchange', 'symbol',)
     # Add other configurations as needed
-# Import the correct CustomUser model
+
+
+# admin.py
+from django.contrib import admin
 from .models import CustomUser
 
-# Register the CustomUser model if it's not registered already
 admin.site.register(CustomUser)
